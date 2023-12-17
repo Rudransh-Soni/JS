057ccb09eb8f6330bebbddb3d164088aefdcedab
Rudransh-Soni <137506470+Rudransh-Soni@users.noreply.github.com>
2023-12-17 11:18:41 +0000

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 2001
+let accountEmail = "Rudransh@gmail.com"
+var accountPassword = "hello" 
+
+// Dot't use var in our code because it may create issue in block scope and functional scope.
+// Always use Const and Let in our code.
+
+
+accountCity = "Jaipur"
+accountEmail = "soni@gmail.com"
+accountPassword = "212121"
+accountCity = "Bengluru"
+
+
+console.log(accountId);
+
+console.table([ accountEmail, accountId, accountPassword, accountCity])
