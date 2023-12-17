f8e07feafbea64a0de3e7981105e61d10d6ad72c
Rudransh-Soni <137506470+Rudransh-Soni@users.noreply.github.com>
2023-12-17 12:35:24 +0000

    ConversionOperations

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,33 @@
+let marks = true
+
+// console.log(typeof (marks));
+// console.log(marks);
+
+// Converting String into Number
+
+let ValueInNumber = Number(marks)        // Using this method we can change the Srting into Number.
+// console.log(typeof (ValueInNumber));    // In this step our String is converted into Number.
+// console.log(ValueInNumber);
+
+// "33" = 33
+// "34abc" = NaN (Not a Number)
+// true = 1 
+
+// Now lets Convert Numbers into Boolean.
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn) // Here we have to define to which form we have to convert.
+// console.log(booleanIsLoggedIn);
+
+// "1" = true,  "2" = false
+// "" = false
+// "Soni" = true.
+
+// Converting Number into String 
+
+let somenumber = 100
+// console.log(typeof somenumber);  Here the vale is in the form of a Number datatypes 
+
+let stringNumber = String(somenumber) // Here it changed into String. 
+console.log(stringNumber);
+console.log(typeof stringNumber);
