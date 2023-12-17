cfe11b054d606d79942cdd2e9216e398c8b63d9b
Rudransh-Soni <137506470+Rudransh-Soni@users.noreply.github.com>
2023-12-17 11:17:17 +0000

    Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS code as newly version.
+
+ /* 
+Null  (let a = null)
+Number (let b = 1234)
+String (let c = "Soni")
+Symbol (let d = Symbol("Hello"))
+Boolean (let e = true & false)
+BigInt  (let f = BigInt("123"))
+Undefined (let g = Undefined))
+*/
+
+// if you want to the Type of any datatype then just use "typeof" 
+
+console.log(typeof true);
+console.log(typeof null);   // the output of NULL datatype will always be "Object" and 
+console.log(typeof undefined);  // the output of UNDEFINED datatype is always "undefined."
