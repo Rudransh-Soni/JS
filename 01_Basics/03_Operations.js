7d3a40dfec25f1841ed3bb12b110c4e2c8b7f813
Rudransh-Soni <137506470+Rudransh-Soni@users.noreply.github.com>
2023-12-24 11:59:22 +0000

    03_Operations

---
+++ 01_Basics/03_Operations.js
@@ -0,0 +1,38 @@
+let value = 8
+let negValue = -value
+
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);  // ** = Powers.
+//console.log(2/10); 
+//console.log(2%3);  // Modules.
+
+
+//console.log(+ true);
+//console.log(+ "");
+
+let gamecounter = 100
+gamecounter++;
+//console.log(gamecounter);
+
+
+let gamecounter1 = 100
+++gamecounter1;
+//console.log(gamecounter1);
+
+// POSTFIX Increment 
+let x = 3;
+const y = x++; // In this case first "x" value will be assign to "y" and after that it will increment and updated to "x".
+              //  
+// console.log(y);
+// console.log(x);
+
+// PREFIX Increment
+let a = 2;
+const b = ++a; // In this case first it will update the value (by ++) then after it will store the value in variable.
+
+console.log(b)
+console.log(a)
