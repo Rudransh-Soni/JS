867789e1838b618cafc78bd627e947e24b82f1ef
Rudransh-Soni <137506470+Rudransh-Soni@users.noreply.github.com>
2023-12-25 18:07:13 +0000

    Comparison

---
+++ 01_Basics/04_Comaprison.js
@@ -0,0 +1,18 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 <= 1);
+//console.log(2 != 1);
+//console.log(2 == 1);
+
+// === Strictly check. It also checks its datatypes.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
