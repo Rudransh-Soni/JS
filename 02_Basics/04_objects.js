1f737c4687b9c2fe8e3b246d3d7cd459af04554d
Rudransh Soni <137506470+Rudransh-Soni@users.noreply.github.com>
2024-05-05 11:47:02 +0000

    04_objects.js

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,58 @@
+// const yt = new Object()  // This is an Singleton Object
+
+const yt = {}  // This is an Nomn-Singleton object.
+
+yt.id = "2001"
+yt.name = "Soni" 
+yt.isLoggedIn = false
+
+// console.log(yt);
+
+const AnotherUser = {
+    email: "Soni@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Rudransh",
+            lastname: "Soni" 
+        }
+    }
+}
+
+// console.log(AnotherUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"A", 2:"B"}
+const obj2 = {3:"C", 4:"D"}
+const obj4 = {5:"E", 6:"F"}
+
+// const returnedTarget = Object.assign(target, source);    // SYNTAX of object assign. and combining
+// const obj3 = Object.assign({}, obj1, obj2, obj4)     // It combines both the objects. So here also "{}" is act like "target" and rest will act like "Source"
+
+const obj3 = {...obj1, ...obj2, ...obj4}    // using SPREAD method we can combine Objects. this method is easy and new.
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+console.log(yt);
+
+console.log(Object.keys(yt));
+console.log(Object.values(yt));
+console.log(Object.entries(yt));
+
+// to check wether that property is present in that Object or not
+
+console.log(yt.hasOwnProperty('isLoggedIn'));
+console.log(yt.hasOwnProperty('isLoggedOut'));
