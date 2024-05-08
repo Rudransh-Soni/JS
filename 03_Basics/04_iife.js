0864b55e4a9e82f8a5a0efbc6a91269c92771aee
Rudransh Soni <137506470+Rudransh-Soni@users.noreply.github.com>
2024-05-08 20:38:07 +0000

    04_iife.js

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE)
+// for protecting our function from global scope pollution we use IIFE
+
+// Syntax:- () ()  in first bracket we protect our function and seconf bracket is used for declaration.
+
+(function one(){        // this is called named IIFE bcz function have its name.
+    console.log('Database Connected');
+})();               // we have to end this IIFE function using ';' semicolon.
+
+
+( (name) => {                       // here we passed one parameter with 'name'
+    console.log(`name is ${name}`);
+} )
+("Rudrash")
