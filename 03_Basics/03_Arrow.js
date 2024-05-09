515135b609347d7dfff8e2f537cb0c8ca7b73003
Rudransh Soni <137506470+Rudransh-Soni@users.noreply.github.com>
2024-05-09 20:01:58 +0000

    03_Arrow.js

--- 03_Basics/03_Arrow.js
+++ 03_Basics/03_Arrow.js
@@ -14,3 +14,42 @@ const user = {
 // user.welcomemessage()
 
 console.log(this);      // if we using 'this' method here then it will print {} means EMPTY. bcz no global scope is ther.
+
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {          //  here we can store a function in a variable also.
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {           // syntax of arrow function.
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2          // this is also a syntax of "Arrow" function
+
+// const addTwo = (num1, num2) => ( num1 + num2 )       // this is also a syntax of "Arrow" function. always use ()()
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
